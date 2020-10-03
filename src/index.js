7b871bb2b04fdc4e19f6963b7f56148886cfd6d9
Natallia22 <zhuravleva.977@yandex.by>
2020-10-03 15:50:53 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array == "" || array == undefined) {
+    return 0; 
+  }
+    var minim = array[0]
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] < minim) {
+      minim = array[i]
+    }
+     } 
+     return minim;
+  }
+
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array == "" || array == undefined) {
+    return 0; 
+  }
+    var maxim = array[0]
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] > maxim) {
+      maxim = array[i]
+    }
+     } 
+     return maxim;
+  }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+  exports.avg = function avg (array) {
+    if (array == '' || array == undefined) {
+      return 0;
+    }
+    var sum = 0;
+    for (var i = 0; i < array.length; i++){
+      sum += array[i];
+    }
+    return sum/array.length;
+  }
